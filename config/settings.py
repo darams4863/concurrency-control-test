d55ce49cdfe845b2368ec94638ffe2fed71c41d2
darams4863 <dev.daram4863@gmail.com>
2024-12-18 14:24:20 +0900

    Fix: settings default value -> empty string

--- config/settings.py
+++ config/settings.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 class Settings:
     class BaseUrl:
-        HOTPARTNERS = os.getenv("HOTPARTNERS_API_BASE_URL")
-        PAYMENT_VALIDATE_SERVER = os.getenv("PAYMENT_VALIDATE_SERVER_API_BASE_URL")
+        HOTPARTNERS = os.getenv("HOTPARTNERS_API_BASE_URL", "")
+        PAYMENT_VALIDATE_SERVER = os.getenv("PAYMENT_VALIDATE_SERVER_API_BASE_URL", "")
 
     class Endpoint:
-        DEFAULT = os.getenv("DEFAULT_ENDPOINT")
+        DEFAULT = os.getenv("DEFAULT_ENDPOINT", "")
